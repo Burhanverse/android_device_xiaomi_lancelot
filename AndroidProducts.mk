398dfe856e9dba6c9f62b7bf7d55f16ae3173926
127223292+Burhanverse@users.noreply.github.com <127223292+Burhanverse@users.noreply.github.com>
2023-07-16 16:25:40 +0530

    lancelot: Init Blaze

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_lancelot.mk
+    $(LOCAL_DIR)/blaze_lancelot.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_lancelot-user \
-    lineage_lancelot-userdebug \
-    lineage_lancelot-eng
+    blaze_lancelot-user \
+    blaze_lancelot-userdebug \
+    blaze_lancelot-eng
--- blaze_lancelot.mk
+++ blaze_lancelot.mk
@@ -12,9 +12,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/lancelot/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/blaze/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_lancelot
+# blaze flags
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_FACE_UNLOCK_SUPPORTED := true
+BLAZE_MAINTAINER := Burhan
+# GApps
+WITH_GAPPS := false
+
+PRODUCT_NAME := blaze_lancelot
 PRODUCT_DEVICE := lancelot
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
